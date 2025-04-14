c4d7cee6358e6a2f234bf1708633f9ee6a28a168
555Jeka555 <2003antyshev@mail.ru>
2025-04-14 20:01:06 +0300

    Набросок

--- 4/2/src/WebGLUtils.ts
+++ 4/2/src/WebGLUtils.ts
@@ -1,22 +1,55 @@
 const vertexShaderSource = `
-  attribute vec3 a_position;
-  uniform mat4 u_matrix;
+    attribute vec4 aVertexPosition;
+    attribute vec4 aVertexColor;
+	attribute vec3 aNormal;
 
-  void main() {
-    gl_Position = u_matrix * vec4(a_position, 1.0);
-  }
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+    uniform mat3 uNormalMatrix;
+
+    varying lowp vec4 vColor;
+    varying vec3 vNormal;
+
+    void main(void) {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      vColor = aVertexColor;
+      vNormal = normalize(uNormalMatrix * aNormal);
+    }
 `
 
 const fragmentShaderSource = `
-  precision mediump float;
-  uniform vec4 u_color;
-  
-  void main() {
-    gl_FragColor = u_color;
-  }
+	precision mediump float;
+
+    varying lowp vec4 vColor;
+
+	varying vec3 vNormal;
+	uniform vec3 uReverseLightDirection;
+	
+    void main(void) {
+      vec3 normal = normalize(vNormal);
+      normal = gl_FrontFacing ? normal : -normal;
+      float light = max(dot(normal, uReverseLightDirection), 0.0);
+      gl_FragColor = vColor;
+      gl_FragColor.rgb *= light;
+    }
 `
 
-export const createShaderProgram = (gl: WebGLRenderingContext): WebGLProgram => {
+const compileShader = (gl: WebGLRenderingContext, type: number, source: string): WebGLShader => {
+	const shader = gl.createShader(type)
+	if (!shader) {
+		throw new Error('Не удалось создать шейдер')
+	}
+	gl.shaderSource(shader, source)
+	gl.compileShader(shader)
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		const err = gl.getShaderInfoLog(shader)
+		gl.deleteShader(shader)
+		throw new Error('Ошибка компиляции шейдера: ' + err)
+	}
+	return shader
+}
+
+const createShaderProgram = (gl: WebGLRenderingContext): WebGLProgram => {
 	const vertexShader = compileShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
 	const fragmentShader = compileShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
 
@@ -34,17 +67,6 @@ export const createShaderProgram = (gl: WebGLRenderingContext): WebGLProgram => 
 	return program
 }
 
-const compileShader = (gl: WebGLRenderingContext, type: number, source: string): WebGLShader => {
-	const shader = gl.createShader(type)
-	if (!shader) {
-		throw new Error('Не удалось создать шейдер')
-	}
-	gl.shaderSource(shader, source)
-	gl.compileShader(shader)
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-		const err = gl.getShaderInfoLog(shader)
-		gl.deleteShader(shader)
-		throw new Error('Ошибка компиляции шейдера: ' + err)
-	}
-	return shader
+export {
+	createShaderProgram,
 }
--- 4/2/src/main.ts
+++ 4/2/src/main.ts
@@ -1,171 +1,88 @@
-import './index.css'
-import {mat4, vec3} from 'gl-matrix'
+import {MobiusStrip} from './MobiusStrip/MobiusStrip'
+import {Settings} from './Settings/Settings'
 import {createShaderProgram} from './WebGLUtils'
-import {Player} from './Player'
-import {Labyrinth} from './Labyrinth.ts'
-import {LabyrinthView} from "./LabyrinthView.ts";
 
 class App {
-    private readonly canvas: HTMLCanvasElement
-    private readonly gl: WebGLRenderingContext
-    private readonly program: WebGLProgram
-    private readonly labyrinth: Labyrinth
-    private readonly mazeView: LabyrinthView
-    private player: Player
+	private readonly canvas: HTMLCanvasElement
+	private readonly gl: WebGLRenderingContext
+	private readonly program: WebGLProgram
+	private mobiusStrip: MobiusStrip
 
-    private cubeVertexBuffer: WebGLBuffer | null = null
-    private cubeIndexBuffer: WebGLBuffer | null = null
-    private countCubeIndex: number = 0
+	private isMouseDown = false
+	private lastMouseX = 0
+	private lastMouseY = 0
+	private cameraRotationX = 0
+	private cameraRotationY = 0
 
-    private keysUp: { [key: string]: boolean } = {}
-    private lastTime: number = 0
+	private lightIntensity = 1
 
-    constructor() {
-        this.canvas = document.createElement('canvas')
-        document.body.appendChild(this.canvas)
-        this.canvas.width = window.innerWidth
-        this.canvas.height = window.innerHeight
-        const gl = this.canvas.getContext('webgl')
-        if (!gl) throw new Error('WebGL не поддерживается')
-        this.gl = gl
+	constructor() {
+		const settings = new Settings(intensity => {
+			this.lightIntensity = intensity
+		})
+		document.querySelector('#root')?.appendChild(settings.getElement())
 
-        this.program = createShaderProgram(gl)
-        gl.useProgram(this.program)
+		this.canvas = document.createElement('canvas')
+		document.body.appendChild(this.canvas)
+		this.canvas.width = window.innerWidth
+		this.canvas.height = window.innerHeight
+		const gl = this.canvas.getContext('webgl')
+		if (!gl) {
+			throw new Error('WebGL не поддерживается')
+		}
+		this.gl = gl
+		this.program = createShaderProgram(gl)
+		this.mobiusStrip = new MobiusStrip(gl, this.program)
 
-        const matrixLoc = gl.getUniformLocation(this.program, 'u_matrix')
-        const colorLoc = gl.getUniformLocation(this.program, 'u_color')
-        if (!matrixLoc || !colorLoc) throw new Error('Не удалось получить uniform-переменные')
+		window.addEventListener('resize', this.resizeCanvas)
 
-        this.labyrinth = new Labyrinth()
-        this.player = new Player()
-        this.mazeView = new LabyrinthView(this.labyrinth, this.gl, matrixLoc, colorLoc)
-        this.initCubeBuffers()
-        this.setupEventListeners()
-    }
+		this.canvas.addEventListener('mousedown', this.onMouseDown)
+		this.canvas.addEventListener('mousemove', this.onMouseMove)
+		this.canvas.addEventListener('mouseup', this.onMouseUp)
+		this.canvas.addEventListener('mouseleave', this.onMouseUp)
+	}
 
-    run() {
-        this.render(this.lastTime)
-    }
+	render = () => {
+		this.mobiusStrip.render(this.cameraRotationX, this.cameraRotationY, this.lightIntensity)
+		requestAnimationFrame(this.render)
+	}
 
-    private render = (time: number) => {
-        const deltaTime = (time - this.lastTime) / 1000
-        this.lastTime = time
-        requestAnimationFrame(this.render)
-        this.gl.enable(this.gl.DEPTH_TEST)
-        this.gl.clearColor(0.2, 0.2, 0.2, 1)
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT)
+	private resizeCanvas = () => {
+		this.canvas.width = window.innerWidth
+		this.canvas.height = window.innerHeight
+		this.gl.viewport(0, 0, window.innerWidth, window.innerHeight)
+	}
 
-        this.updatePlayer(deltaTime)
+	private onMouseDown = (event: MouseEvent) => {
+		this.isMouseDown = true
+		this.lastMouseX = event.clientX
+		this.lastMouseY = event.clientY
+	}
 
-        const projectionMatrix = this.calcProjectionMatrix()
-        const viewMatrix =  this.calcViewMatrix()
-        this.mazeView.draw(this.countCubeIndex, projectionMatrix, viewMatrix)
-    }
+	private onMouseMove = (event: MouseEvent) => {
+		if (!this.isMouseDown) {
+			return
+		}
 
-    private updatePlayer(deltaTime: number) {
-        if (this.keysUp['ArrowUp']) {
-            this.player.moveForward(this.labyrinth, deltaTime)
-        }
-        if (this.keysUp['ArrowDown']){
-            this.player.moveForward(this.labyrinth, -deltaTime)
-        }
-        if (this.keysUp['ArrowLeft']) {
-            this.player.rotate(-deltaTime)
-        }
-        if (this.keysUp['ArrowRight']) {
-            this.player.rotate(deltaTime)
-        }
-        if (this.keysUp['x']) {
-            this.player.setPitch(deltaTime * this.player.rotationSpeed)
-        }
-        if (this.keysUp['z']) {
-            this.player.setPitch(-(deltaTime * this.player.rotationSpeed))
-        }
-    }
+		const deltaX = event.clientX - this.lastMouseX
+		const deltaY = event.clientY - this.lastMouseY
+		this.lastMouseX = event.clientX
+		this.lastMouseY = event.clientY
 
-    private initCubeBuffers() {
-        const gl = this.gl
-        const vertices = new Float32Array([
-            0, 0, 0,   1, 0, 0,   1, 1, 0,   0, 1, 0,
-            0, 0, 1,   1, 0, 1,   1, 1, 1,   0, 1, 1,
-        ])
-        const indices = new Uint16Array([
-            4, 5, 6, 4, 6, 7,  // Передняя грань
-            0, 2, 1, 0, 3, 2,  // Задняя грань
-            0, 7, 3, 0, 4, 7,  // Левая грань
-            1, 2, 6, 1, 6, 5,  // Правая грань
-            3, 6, 2, 3, 7, 6,  // Верхняя грань
-            0, 1, 5, 0, 5, 4,  // Нижняя грань
-        ])
-        this.countCubeIndex = indices.length
+		const sensitivity = 0.001
+		this.cameraRotationY -= deltaX * sensitivity
+		this.cameraRotationX += deltaY * sensitivity
 
-        this.cubeVertexBuffer = gl.createBuffer()!
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.cubeVertexBuffer)
-        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+		const maxPitch = Math.PI / 2 - 0.01
+		const minPitch = -maxPitch
+		this.cameraRotationX = Math.max(minPitch, Math.min(maxPitch, this.cameraRotationX))
+	}
 
-        this.cubeIndexBuffer = gl.createBuffer()!
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.cubeIndexBuffer)
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
 
-        const aPositionLocation = gl.getAttribLocation(this.program, 'a_position')
-        if (aPositionLocation === -1) {
-            throw new Error('Атрибут "a_position" не найден')
-        }
-        gl.enableVertexAttribArray(aPositionLocation)
-        gl.vertexAttribPointer(aPositionLocation, 3, gl.FLOAT, false, 0, 0)
-    }
-
-    private setupEventListeners() {
-        window.addEventListener('resize', this.resizeCanvas)
-        window.addEventListener('keydown', this.handleKeyDown)
-        window.addEventListener('keyup', this.handleKeyUp)
-    }
-
-    private resizeCanvas = () => {
-        this.canvas.width = window.innerWidth
-        this.canvas.height = window.innerHeight
-        this.gl.viewport(0, 0, window.innerWidth, window.innerHeight)
-    }
-
-    private handleKeyDown = (event: KeyboardEvent) => {
-        this.keysUp[event.key] = true
-    }
-
-    private handleKeyUp = (event: KeyboardEvent) => {
-        this.keysUp[event.key] = false
-    }
-
-    private calcProjectionMatrix() {
-        const projectionMatrix = mat4.create()
-        const fov = (60 * Math.PI) / 180
-        const aspect = this.canvas.width / this.canvas.height
-        const near = 0.1
-        const far = 100
-        mat4.perspective(projectionMatrix, fov, aspect, near, far)
-
-        return projectionMatrix
-    }
-
-    private calcViewMatrix() {
-        const viewMatrix = mat4.create();
-        const eye = vec3.fromValues(
-            this.player.position[0],
-            this.player.position[1],
-            this.player.position[2]
-        );
-
-        const center = vec3.fromValues(
-            eye[0] + Math.cos(this.player.direction) * Math.cos(this.player.pitch),
-            eye[1] + Math.sin(this.player.pitch),
-            eye[2] + Math.sin(this.player.direction) * Math.cos(this.player.pitch)
-        );
-
-        const up = vec3.fromValues(0, 1, 0);
-        mat4.lookAt(viewMatrix, eye, center, up);
-
-        return viewMatrix;
-    }
+	private onMouseUp = () => {
+		this.isMouseDown = false
+	}
 }
 
 const app = new App()
-app.run()
+requestAnimationFrame(app.render)
